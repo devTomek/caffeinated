5c13b6c83b9932a15ba3eb3b89a312077d8bcf8f
Tomasz Chybzinski <tomasz.chybzinski@gmail.com>
2019-10-28 23:27:59 +0100

    use useQuery, fix npm start

--- client/src/components/dashboard/DashboardContainer.js
+++ client/src/components/dashboard/DashboardContainer.js
@@ -3,6 +3,8 @@ import Dashboard from "./Dashboard";
 import { connect } from "react-redux";
 import loginActions from "../../actions/loginActions";
 import { useHistory } from "react-router-dom";
+import gql from "graphql-tag";
+import { useQuery } from "@apollo/react-hooks";
 
 const mapDispatchToProps = dispatch => ({
     logout: () => dispatch(loginActions.logout())
@@ -23,7 +25,25 @@ const DashboardContainer = ({ logout }) => {
         history.push("/auth");
     };
 
-    return <Dashboard logout={doLogout} />;
+    const getUsers = gql`
+        {
+            users {
+                _id
+                email
+            }
+        }
+    `;
+
+    // todo: attach Auth header, probably in Apollo config
+    const { loading, error, data } = useQuery(getUsers);
+
+    if (loading) return "Loading...";
+    if (error) return `Error! ${error.message}`;
+
+    console.log(data);
+    const users = data.users;
+
+    return <Dashboard logout={doLogout} users={users} />;
 };
 
 export default connect(
